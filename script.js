c7ed6baf034fab17c3c71014943b600ecd87fcac
Aland <aland.zland@gmail.com>
2024-03-05 12:42:09 +0300

    sherash haw!!!

---
+++ script.js
@@ -0,0 +1,6 @@
+console.log("Hello, World!");
+// 1. Write a JavaScript function that reverse a number.
+// function reverseNumber(num) {
+//   return parseFloat(num.toString().split("").reverse().join(""));
+// }
+// console.log(reverseNumber(12));
